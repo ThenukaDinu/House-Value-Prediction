c9c7e98d028a10b6483a95c79765d96401901a0a
Thenuka Dinu Jayarathna <thenukadinu@gmail.com>
2023-05-02 03:31:12 +0530

    add welcome page video and poster

--- Clients/WebApp/src/views/HomeView.vue
+++ Clients/WebApp/src/views/HomeView.vue
@@ -2,7 +2,12 @@
 
 <template>
   <div class="home_page">
-    <h1>Welcome</h1>
+    <video poster="welcome-image.png" playsinline autoplay muted loop>
+      <source src="background_video_1.mp4" type="video/mp4" />
+    </video>
+    <div id="fashion">
+      <h2>Discover the True Value of Your House with Our Accurate Prediction Tool</h2>
+    </div>
   </div>
 </template>
 
@@ -11,6 +16,56 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: calc(100vh - 54px);
+}
+
+@font-face {
+  font-family: Moderne Sans;
+  src: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/moderne_sans.woff2);
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  background: #000;
+  background-size: cover;
+}
+video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+div#fashion {
+  min-height: calc(100vh - 54px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  font-family: Moderne Sans, sans-serif;
+  text-align: center;
+  font-size: 2rem;
+  width: 100%;
+  letter-spacing: 0.5rem;
+}
+nav a {
+  text-decoration: none;
+  color: inherit;
+  padding: 1rem;
+}
+h2 {
+  font-family: Century Schoolbook, Century Schoolbook L, Georgia, serif;
+  font-size: 8vmin;
+  text-align: center;
+  margin: 2rem 3rem 0;
+  mix-blend-mode: overlay;
+  color: #f3f3f3;
+  font-weight: 100;
 }
 </style>
